46d3de940aecb9782915e1282d0ee5cc73fdabe8
vanness <280723148@qq.com>
2016-12-22 14:56:47 +0800

    python manage.py makemigrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pools',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(   , 'db.sqlite3'),
+        'NAME': os.path.join( BASE_DIR, 'db.sqlite3'),
     }
 }
 
